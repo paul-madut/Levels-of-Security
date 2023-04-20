54a1b79518a20d9101bc78197a3c1d7bcea150fe
paul-madut <paul.madut@mths.ca>
2023-04-20 11:10:12 -0400

    Base Files

---
+++ frontend/diary-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Route, BrowserRouter, Link, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
